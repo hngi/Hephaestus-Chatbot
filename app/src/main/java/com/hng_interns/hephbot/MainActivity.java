0711a0c875ef13bb59ec478a11c444834b4e2b50
mitchelletakuro <mitchelletakuro@gmail.com>
2019-09-25 21:19:27 +0100

    About and Splash screen

--- app/src/main/java/com/hng_interns/hephbot/MainActivity.java
+++ app/src/main/java/com/hng_interns/hephbot/MainActivity.java
@@ -2,7 +2,10 @@ package com.hng_interns.hephbot;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button chatbtn = (Button) findViewById(R.id.button);
+        Button aboutbtn = (Button) findViewById(R.id.button2);
+
+        chatbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent int1= new Intent(MainActivity.this,  HephBot.class;
+                startActivity(int1);
+            }
+        });
+
+
+        aboutbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent int2= new Intent(MainActivity.this,AboutActivity.class);
+                startActivity(int2);
+            }
+        });
+
     }
 }
