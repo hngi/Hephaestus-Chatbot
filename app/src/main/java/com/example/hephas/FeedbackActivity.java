83882237aa249df23f1097c71ff16792d6e97c45
mitchelletakuro <mitchelletakuro@gmail.com>
2019-10-05 23:47:06 +0100

    build update

--- app/src/main/java/com/example/hephas/FeedbackActivity.java
+++ app/src/main/java/com/example/hephas/FeedbackActivity.java
@@ -2,6 +2,7 @@ package com.example.hephas;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -28,6 +29,7 @@ public class FeedbackActivity extends AppCompatActivity implements View.OnClickL
         switch (v.getId()) {
             case R.id.ButtonSendFeedback:
                 // Do something
+                
         }
     }
 
