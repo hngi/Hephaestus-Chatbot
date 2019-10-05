f60d67c68ad9c4e9a87f4bf5020556d689e41229
mitchelletakuro <mitchelletakuro@gmail.com>
2019-10-05 18:48:00 +0100

    minor changes

--- app/build/generated/not_namespaced_r_class_sources/debug/r/de/hdodenhof/circleimageview/R.java
+++ app/build/generated/not_namespaced_r_class_sources/debug/r/de/hdodenhof/circleimageview/R.java
@@ -12,16 +12,16 @@ public final class R {
     public static final class attr {
         private attr() {}
 
-        public static final int civ_border_color = 0x7f040084;
-        public static final int civ_border_overlay = 0x7f040085;
-        public static final int civ_border_width = 0x7f040086;
-        public static final int civ_circle_background_color = 0x7f040087;
-        public static final int civ_fill_color = 0x7f040088;
+        public static final int civ_border_color = 0x7f030052;
+        public static final int civ_border_overlay = 0x7f030053;
+        public static final int civ_border_width = 0x7f030054;
+        public static final int civ_circle_background_color = 0x7f030055;
+        public static final int civ_fill_color = 0x7f030056;
     }
     public static final class styleable {
         private styleable() {}
 
-        public static final int[] CircleImageView = { 0x7f040084, 0x7f040085, 0x7f040086, 0x7f040087, 0x7f040088 };
+        public static final int[] CircleImageView = { 0x7f030052, 0x7f030053, 0x7f030054, 0x7f030055, 0x7f030056 };
         public static final int CircleImageView_civ_border_color = 0;
         public static final int CircleImageView_civ_border_overlay = 1;
         public static final int CircleImageView_civ_border_width = 2;
